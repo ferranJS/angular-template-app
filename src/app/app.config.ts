ad72ecb313dc7d836350eb63779c23d66cef304b
Ferran <ferranramirezmarti@gmail.com>
2023-06-07 12:44:43 +0200

    for conflict

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,14 @@
-import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  isDevMode
+} from "@angular/core"
+import { EffectsModule } from "@ngrx/effects"
+import { StoreModule } from "@ngrx/store"
+import { StoreDevtoolsModule } from "@ngrx/store-devtools"
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,4 +18,4 @@ export const appConfig: ApplicationConfig = {
       EffectsModule.forRoot([])
     )
   ]
-};
+}
